0c13514e886fb0a5081d1f0c5b623884e2376d1d
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-13 23:08:38 +0100

    feat(ch02): use summary matching the temperature

--- 02/UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ 02/UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -11,6 +11,17 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private string MapFeelToTemp(int temperatureC)
+    {
+        if (temperatureC <= 0)
+        {
+            return Summaries.First();
+        }
+
+        var summariesIndex = temperatureC / 5 + 1;
+        return summariesIndex >= Summaries.Length ? Summaries.Last() : Summaries[summariesIndex];
+    }
+    
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,15 +29,18 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetRandomWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index =>
+        {
+            var temperatureC = Random.Shared.Next(-20, 55);
+            return new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                TemperatureC = temperatureC,
+                Summary = MapFeelToTemp(temperatureC)
+            };
+        }).ToArray();
     }
 }
