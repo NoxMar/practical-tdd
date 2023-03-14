77c8d6a061026b7193f3817a6ef0bde663b3db89
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-14 22:32:38 +0100

    test(ch02): conversion c to f using di

---
+++ 02/UqsWeather/Uqs.Weather.TestRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Uqs.Weather.Controllers;
+
+NullLogger<WeatherForecastController> logger = new();
+WeatherForecastController controller = new(logger, null!);
+
+var f1 = controller.ConvertCToF(-1);
+// ReSharper disable once CompareOfFloatsByEqualityOperator
+if (f1 != 30.20d)
+{
+    throw new Exception("Invalid");
+}
+var f2 = controller.ConvertCToF(1.2);
+// ReSharper disable once CompareOfFloatsByEqualityOperator
+if (f2 != 34.16d)
+{
+    throw new Exception("Invalid");
+}
+Console.WriteLine("Test Passed");
