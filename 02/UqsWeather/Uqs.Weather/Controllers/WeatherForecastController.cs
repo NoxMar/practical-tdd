c6c8cbf7288c31012374e1bd9869754aedfab748
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-14 22:38:59 +0100

    refactor(ch02): inject openweather web api client

--- 02/UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
+++ 02/UqsWeather/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -25,11 +25,11 @@ public class WeatherForecastController : ControllerBase
     }
     
     private readonly ILogger<WeatherForecastController> _logger;
-    private readonly IConfiguration _config;
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
+    private readonly IClient _client;
+    public WeatherForecastController( IClient client, ILogger<WeatherForecastController> logger)
     {
+        _client = client;
         _logger = logger;
-        _config = config;
     }
 
     [HttpGet("GetRandomWeatherForecast")]
@@ -52,10 +52,7 @@ public class WeatherForecastController : ControllerBase
     {
         const decimal GREENWICH_LAT = 51.4810m;
         const decimal GREENWICH_LON = 0.0052m;
-        string apiKey = _config["OpenWeather:Key"];
-        HttpClient httpClient = new();
-        Client openWeatherClient = new(apiKey, httpClient);
-        OneCallResponse res = await openWeatherClient.OneCallAsync
+        OneCallResponse res = await _client.OneCallAsync
         (GREENWICH_LAT, GREENWICH_LON, new [] {
             Excludes.Current, Excludes.Minutely,
             Excludes.Hourly, Excludes.Alerts }, Units.Metric);
--- 02/UqsWeather/Uqs.Weather/Program.cs
+++ 02/UqsWeather/Uqs.Weather/Program.cs
@@ -1,7 +1,15 @@
+using AdamTibi.OpenWeather;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Add the OpenWeather API client service
+builder.Services.AddSingleton<IClient>(_ =>
+{
+    var apiKey = builder.Configuration["OpenWeather:Key"]!;
+    HttpClient httpClient = new();
+    return new Client(apiKey, httpClient);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
