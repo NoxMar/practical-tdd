376540b4f974d1d439ca723b4cd96207396acef1
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-17 20:09:16 +0100

    test(ch05): test length validation on changing username

--- 05/TddByExample/Uqs.Customer.Test.Unit/ProfileServiceTests.cs
+++ 05/TddByExample/Uqs.Customer.Test.Unit/ProfileServiceTests.cs
@@ -16,4 +16,31 @@ public class ProfileServiceTests
         Assert.Equal("username", ex.ParamName);
         Assert.StartsWith("Null", ex.Message);
     }
+
+    [Theory(Skip = "TODO")]
+    [InlineData("AnameOf8", true)]
+    [InlineData("NameOfChar12", true)]
+    [InlineData("AnameOfChar13", false)]
+    [InlineData("NameOf7", false)]
+    [InlineData("", false)]
+    public void ChangeUsernameVariousLengthUsernames_ArgumentOutOfRangeExceptionIfInvalid(string username,
+        bool isValid)
+    {
+        // Arrange
+        var sut = new ProfileService();
+        
+        // Act
+        var e = Record.Exception(() => sut.ChangeUsername(username));
+        
+        // Assert
+        if (isValid)
+        {
+            Assert.Null(e);
+            return;
+        }
+
+        var ex = Assert.IsType<ArgumentOutOfRangeException>(e);
+        Assert.Equal("username", ex.ParamName);
+        Assert.StartsWith("Length", ex.Message);
+    }
 }
