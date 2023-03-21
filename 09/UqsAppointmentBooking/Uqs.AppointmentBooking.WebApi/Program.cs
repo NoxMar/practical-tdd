50895ba001aa592ef29073651a965a4289a28b02
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-21 23:34:55 +0100

    feat(ch09): wire db for api

--- 09/UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
+++ 09/UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Uqs.AppointmentBooking.Domain.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentBooking")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
