4f1b83771d33df8a1a929bb014f48be496860bc3
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-26 04:57:31 +0200

    chore(ch10): setup web-related projects

--- 10/UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
+++ 10/UqsAppointmentBooking/Uqs.AppointmentBooking.WebApi/Program.cs
@@ -1,20 +1,53 @@
+using Microsoft.Azure.Cosmos;
+using Uqs.AppointmentBooking.Domain;
+using Uqs.AppointmentBooking.Domain.Database;
+using Uqs.AppointmentBooking.Domain.Repositories;
+using Uqs.AppointmentBooking.Domain.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add DB Services
+builder.Services.AddSingleton<CosmosClient>(
+    new CosmosClient(builder.Configuration.GetConnectionString("AppointmentBooking"), 
+        new CosmosClientOptions
+        {
+            SerializerOptions = new CosmosSerializationOptions
+            {
+                PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+            }
+        }));
+builder.Services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddSingleton<IServiceRepository, ServiceRepository>();
+builder.Services.AddSingleton<ICustomerRepository, CustomerRepository>();
+builder.Services.AddSingleton<IAppointmentRepository, AppointmentRepository>();
 
+// Add services to the container.
+builder.Services.AddScoped<INowService, NowService>();
+builder.Services.AddScoped<IServicesService, ServicesService>();
+builder.Services.AddScoped<IEmployeesService, EmployeesService>();
+builder.Services.AddScoped<ISlotsService, SlotsService>();
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection(nameof(ApplicationSettings)));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        await SeedData.Initialize(services);
+    }
 }
+
+// Configure the HTTP request pipeline.
+
+// Enable CORS
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(_ => true) // allow any origin  
+    .AllowCredentials());               // allow credentials 
 
 app.UseHttpsRedirection();
 
