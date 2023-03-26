b4c32373fabd3891c957bfce6c23be041a16ea41
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-03-26 13:25:47 +0200

    test(ch10): test `ServicesService` for empty services

---
+++ 10/UqsAppointmentBooking/Uqs.AppointmentBooking.Domain.Tests.Unit/ServicesServiceTests.cs
@@ -0,0 +1,24 @@
+using NSubstitute;
+using Uqs.AppointmentBooking.Domain.Repositories;
+using Uqs.AppointmentBooking.Domain.Services;
+
+namespace Uqs.AppointmentBooking.Domain.Tests.Unit;
+
+public class ServicesServiceTests
+{
+    private readonly IServiceRepository _serviceRepository = Substitute.For<IServiceRepository>();
+    private ServicesService? _sut;
+
+    [Fact(Skip = "TODO")]
+    public async Task GetActiveServices_NoServiceInTheSystem_NoServices()
+    {
+        // Arrange
+        _sut = new ServicesService(_serviceRepository);
+        
+        // Act
+        var actual = await _sut.GetActiveServices();
+        
+        // Assert
+        Assert.Empty(actual);
+    }
+}
---
+++ 10/UqsAppointmentBooking/Uqs.AppointmentBooking.Domain/Services/ServicesService.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using Uqs.AppointmentBooking.Domain.Repositories;
+
+namespace Uqs.AppointmentBooking.Domain.Services;
+
+public class ServicesService
+{
+    private readonly IServiceRepository _serviceRepository;
+    public ServicesService(IServiceRepository serviceRepository)
+    {
+        _serviceRepository = serviceRepository;
+    }
+
+    public async Task<IEnumerable> GetActiveServices()
+    {
+        throw new NotImplementedException();
+    }
+}
